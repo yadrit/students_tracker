eaeaa66b36dbd6810b0775b4190ee4b84808bbea
yadrit <yadritig@gmail.com>
2020-01-10 21:15:22 +0200

    Changed Forms - Added GroupsAddForm class

--- src/students/forms.py
+++ src/students/forms.py
@@ -6,3 +6,9 @@ class StudentsAddForm(ModelForm):
     class Meta:
         model = Student
         fields = '__all__'
+
+
+class GroupsAddForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = '__all__'
